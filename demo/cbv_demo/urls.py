68288ce43bb5ca2602c0d0c5529193046bb906e0
M Haziq <m_haziq@outlook.com>
2017-10-12 19:28:36 +0500

    minor changes

--- demo/cbv_demo/urls.py
+++ demo/cbv_demo/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-
 from .views import ContactData
+
 
 urlpatterns = [
     url(r'^contact', ContactData.as_view(), name='contact'),
--- demo/fbv_demo/views.py
+++ demo/fbv_demo/views.py
@@ -1,5 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Medical
 from rest_framework import status
